ed95e7de992e3e4cfb54d0cbcaed2ce4b7d2095e
Yurgenich87 <yurakarbushev@gmail.com>
2023-08-10 12:52:28 +0700

    Edit Task_1

--- Home_work_2/Task_1/assignment_1.py
+++ Home_work_2/Task_1/assignment_1.py
@@ -1,14 +1,15 @@
 def get_float_input():
+    """Ввод дробного числа пользователем"""
     while True:
         try:
             user_input = float(input("Введите дробное число: "))
             if user_input.is_integer():
                 print("Ошибка! Введите дробное число, а не целое.")
             else:
-                return user_input
+                return print(f"Вы ввели дробное число: {user_input}")
         except ValueError:
             print("Ошибка! Введите корректное дробное число.")
 
-# Пример использования
-user_float = get_float_input()
-print(f"Вы ввели: {user_float}")
+
+if __name__ == '__main__':
+    get_float_input()
