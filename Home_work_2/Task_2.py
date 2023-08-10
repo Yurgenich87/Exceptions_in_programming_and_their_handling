73c3251274deba292d66f718821483f88f12d1e1
Yurgenich87 <yurakarbushev@gmail.com>
2023-08-10 11:01:55 +0700

    Add Task_2 in Home work 2

---
+++ Home_work_2/Task_2.py
@@ -0,0 +1,15 @@
+
+def get_float_input():
+    while True:
+        try:
+            user_input = float(input("Введите дробное число: "))
+            if user_input.is_integer():
+                print("Ошибка! Введите дробное число, а не целое.")
+            else:
+                return user_input
+        except ValueError:
+            print("Ошибка! Введите корректное дробное число.")
+
+# Пример использования
+user_float = get_float_input()
+print(f"Вы ввели: {user_float}")
